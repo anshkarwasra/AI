8be3d285bf88db51f816c530890cb69248475d40
Ansh Karwasra <anshkarwasra15@gmail.com>
2024-10-29 23:51:39 +0530

    first commit

---
+++ NeuralNetwork from scratch/asdf.py
@@ -0,0 +1,16 @@
+print("hello world")
+
+
+def factorial(n):
+    alpha = 1
+    for i in range(1,n+1):
+        alpha = alpha*i
+    return alpha
+
+def this():
+    print("factroial of 5 loading......")
+    print("still loading........")
+    print(".......")
+    print(factorial(5))
+
+this()
